caf271c309fa56bb8e99e9de65151e4795eca019
Neel Doshi <neeldoshi1477@gmail.com>
2021-06-05 20:05:52 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const photo-btn =document.querySelector(".photo-btn");
+const image = document.querySelector(".image");
+function generate-images(){
+    i=0;
+    image.innerHTML = <img src ="https:picsum.photos/200/300?random=1">;
+    
+};
+
+return generate-images;
+photo-btn.addEventListener("click",function(generate-images){});
