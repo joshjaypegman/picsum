f452c28342f691bc8047bd8533a66a015b521998
Neel Doshi <neeldoshi1477@gmail.com>
2021-06-06 20:28:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const photo_btn = document.querySelector(".photo_btn");
 const images = document.querySelector(".images");
 
-photo_btn.addEventListener("click", generate_images);
+photo_btn.addEventListener("click", generate_images());
 function generate_images(){
     console.log("111");
     images.innerHTML = "<p>Hii</p>";
     
 };
-// generate_images();
+generate_images();
